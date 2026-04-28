4f094452444a04ed5f1377fdc971ddeec16172fe
김예안 <kjan0929@naver.com>
2026-04-28 19:21:52 +0900

    docs(storybook): InputField 스토리 작성
    Co-authored-by: Eastar-DS <dororo0104@naver.com>

--- src/components/InputField.stories.ts
+++ src/components/InputField.stories.ts
@@ -1,51 +1,53 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { userEvent, within } from 'storybook/test';
 
 import { fn } from 'storybook/test';
 
 import InputField from './InputField';
 
 const meta = {
-  title: 'Example/InputField',
+  title: 'InputField',
   component: InputField,
   parameters: {
     layout: 'centered',
+    docs: {                                                                                                                                                                                       
+        description: {                                                                                                                                                                              
+          component: '카드 정보를 입력받는 공용 Input 컴포넌트입니다.',
+        },                                                                                                                                                                                          
+      }, 
   },
 
   tags: ['autodocs'],
 
   argTypes: {
-    backgroundColor: { control: 'color' },
+    isError: { control: 'boolean' },
+    placeholder: { table: { disable: true } },
   },
 
-  args: { onClick: fn() },
+  args: { onClick: fn(), placeholder: '카드 번호를 입력해주세요' },
 } satisfies Meta<typeof InputField>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    isError: false,
   },
 };
 
-export const Secondary: Story = {
+export const Focus: Story = {
   args: {
-    label: 'Button',
+    isError: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('textbox'));
   },
 };
 
-export const Large: Story = {
+export const Error: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    isError: true,
   },
 };
