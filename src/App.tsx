4022bd55aab29dd42c5a3f140dfbc0eaa0299208
김예안 <kjan0929@naver.com>
2026-04-29 16:50:56 +0900

    feat(style): layout 적용
    Co-authored-by: Eastar-DS <Eastar-DS@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import styled from '@emotion/styled';
+import ServiceLayout from './components/ServiceLayout/ServiceLayout';
+
 function App() {
-  return <div>React PaymentsPayments</div>;
+  return (
+    <RootLayout>
+      <ServiceLayout />
+    </RootLayout>
+  );
 }
+
+const RootLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+`;
 
 export default App;
---
+++ src/components/ServiceLayout/ServiceLayout.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export default function ServiceLayout() {
+  return <Layout>RootLayout</Layout>;
+}
+
+const Layout = styled.div`
+  width: 376px;
+  height: 700px;
+`;
