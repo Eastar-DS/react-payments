9f04228007f6cfa577b4c9a65453786904629fe8
김예안 <kjan0929@naver.com>
2026-04-30 19:49:49 +0900

    chore(style): 카드 번호 컴포넌트 마스킹 스타일 변경
    Co-authored-by: Eastar-DS <Eastar-DS@users.noreply.github.com>

--- src/components/CardPreview/CardNumbers.tsx
+++ src/components/CardPreview/CardNumbers.tsx
@@ -1,18 +1,23 @@
 import styled from '@emotion/styled';
 
 interface CardNumbersProps {
-  numbers: number[];
+  numbers: string;
   index: number;
 }
 
 export default function CardNumbers({ numbers, index }: CardNumbersProps) {
-  const formattedNumbers = index > 1 ? numbers.map(() => '•').join('') : numbers.join('');
+  const isMasked = index > 1;
+  const formattedNumbers = isMasked ? '•'.repeat(numbers.length) : numbers;
 
-  return <Numbers>{formattedNumbers}</Numbers>;
+  return <Numbers isMasked={isMasked}>{formattedNumbers}</Numbers>;
 }
 
-const Numbers = styled.div`
+const Numbers = styled.div<{ isMasked: boolean }>`
+  display: flex;
+  align-items: center;
+  width: 35px;
   font-size: 14px;
   font-weight: 500;
+  letter-spacing: ${({ isMasked }) => (isMasked ? '-4px' : 'normal')};
   color: #fff;
 `;
