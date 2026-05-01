cd1d08b70b49142afa40c5f7dff288fc69f0a075
김예안 <kjan0929@naver.com>
2026-05-01 11:06:34 +0900

    chore(layout): InputField 레이아웃 내부 에러 메시지 컴포넌트 제거
    Co-authored-by: Eastar-DS <Eastar-DS@users.noreply.github.com>

--- src/components/Layout/InputFieldLayout.tsx
+++ src/components/Layout/InputFieldLayout.tsx
@@ -1,18 +1,15 @@
 import styled from '@emotion/styled';
 import SectionTitle from '../Common/SectionTitle/SectionTitle';
 import HintText from '../Common/HintText/HintText';
-import ErrorMessage from '../Common/ErrorMessage/ErrorMessage';
 
 export default function InputFieldLayout({
   sectionTitle,
   hintText,
   children,
-  errorMessage,
 }: {
   sectionTitle: React.ReactNode;
   hintText: React.ReactNode;
   children: React.ReactNode;
-  errorMessage: React.ReactNode;
 }) {
   return (
     <Layout>
@@ -20,10 +17,7 @@ export default function InputFieldLayout({
         <SectionTitle>{sectionTitle}</SectionTitle>
         <HintText>{hintText}</HintText>
       </Header>
-      <ContentWrapper>
-        {children}
-        <ErrorMessage>{errorMessage}</ErrorMessage>
-      </ContentWrapper>
+      <ContentWrapper>{children}</ContentWrapper>
     </Layout>
   );
 }
