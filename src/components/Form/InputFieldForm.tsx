873796a2fbda6f2018b431851dc957872fb5c3f4
김예안 <kjan0929@naver.com>
2026-05-01 11:05:20 +0900

    feat(form): validator 및 에러 메시지 관련 코드 추가
    Co-authored-by: Eastar-DS <Eastar-DS@users.noreply.github.com>

--- src/components/Form/InputFieldForm.tsx
+++ src/components/Form/InputFieldForm.tsx
@@ -1,45 +1,56 @@
 import styled from '@emotion/styled';
 import Label from '../Common/Label/Label';
-import InputField from '../Common/InputField/InputField';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { CardNumbersType, ExpirationDateType } from './PaymentForm';
+import ErrorMessage from '../Common/ErrorMessage/ErrorMessage';
+import FormField from './FormField';
 
 interface InputFieldFormProps<T extends CardNumbersType | ExpirationDateType | string> {
   id: string;
-  inputLength: number;
   label: string;
   value: T;
+  validator: (
+    inputValue: string,
+    index: number
+  ) => { error: boolean; errorMessage: string };
   onChange: (e: ChangeEvent<HTMLInputElement>, index: number) => void;
 }
 
 export default function InputFieldForm<T extends CardNumbersType | ExpirationDateType | string>({
   id,
-  inputLength,
   label,
   value,
+  validator,
   onChange,
 }: InputFieldFormProps<T>) {
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
   const convertValueToStringArray = (value: T): string[] => {
     if (typeof value === 'string') return [value];
     if (Array.isArray(value)) return value;
     return Object.values(value);
   };
 
+  const numberList = convertValueToStringArray(value);
+
   return (
     <FormContainer>
       <Label htmlFor={id}>{label}</Label>
-      
+
       <InputFieldWrapper>
-        {Array.from({ length: inputLength }).map((_, index) => (
-          <InputField
-            isError={false}
+        {Array.from({ length: numberList.length }).map((_, index) => (
+          <FormField
             id={index === 0 ? id : String(index)}
-            type="number"
-            value={convertValueToStringArray(value)[index]}
-            onChange={(e) => onChange(e, index)}
+            index={index}
+            numbers={numberList[index]}
+            validator={(value) => validator(value, index)}
+            onChange={onChange}
+            setErrorMessage={setErrorMessage}
           />
         ))}
       </InputFieldWrapper>
+
+      {<ErrorMessage>{errorMessage.trim().length > 0 && errorMessage}</ErrorMessage>}
     </FormContainer>
   );
 }
