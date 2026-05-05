8c9148f4b4284ebc575446042341df5451af3469
eastar-DS <dororo0104@naver.com>
2026-05-05 19:26:10 +0900

    refactor(type): 벨리데이터 타입과 유효성검사 이후 타입 선언

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,12 @@ export type { InputFieldConfig };
 export type CardNumbersType = [string, string, string, string];
 export type ExpirationDateType = { month: string; year: string };
 export type ExpirationDateListType = [string, string];
+
+export type ValidatorResult = {
+  error: boolean;
+  errorMessage: string;
+};
+
+export type ValidationResult = ValidatorResult & {
+  block: boolean;
+};
