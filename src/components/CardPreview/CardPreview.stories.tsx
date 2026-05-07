9d314c5a1ca7c466e2aea8ba7d4157d0e70a2373
eastar-DS <dororo0104@naver.com>
2026-05-07 22:01:56 +0900

    feat(card-preview): cardBrand/backgroundColor prop 도입, 브랜드 로고 프롭으로 받도록 변경

--- src/components/CardPreview/CardPreview.stories.tsx
+++ src/components/CardPreview/CardPreview.stories.tsx
@@ -39,21 +39,24 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    cardNumberList: ['1234','1234','1234','1234'],
+    cardNumberList: ['1234', '1234', '1234', '1234'],
     expirationDate: '12/31',
+    cardBrand: 'NONE',
   },
 };
 
 export const Visa: Story = {
   args: {
-    cardNumberList: ['4234','1234','1234','1234'],
+    cardNumberList: ['4234', '1234', '1234', '1234'],
     expirationDate: '12/31',
+    cardBrand: 'VISA',
   },
 };
 
 export const Master: Story = {
   args: {
-    cardNumberList: ['5134','1234','1234','1234'],
+    cardNumberList: ['5134', '1234', '1234', '1234'],
     expirationDate: '12/31',
+    cardBrand: 'MASTER',
   },
 };
