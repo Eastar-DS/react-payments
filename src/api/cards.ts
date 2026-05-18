847aed374332963839f39789e78326f3356b6639
eastar-DS <dororo0104@naver.com>
2026-05-18 17:48:42 +0900

    fix(api): getCards의 반환타입(Promise<Card[]>) 작성

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -1,4 +1,6 @@
-export default async function getCards(signal?: AbortSignal) {
+import { Card } from '../types';
+
+export default async function getCards(signal?: AbortSignal): Promise<Card[]> {
   const res = await fetch('/cards', { signal });
   if (!res.ok) throw Error('카드 목록을 불러올 수 없습니다.');
   return res.json();
